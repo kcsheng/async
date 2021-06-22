613a16654c71fb877782e320cd224438c9cd7ee1
kcsheng <kacysheng@mail.com>
2021-06-22 23:46:55 +1000

    Create callbacks to get user's data

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 console.log("Before");
-const user = getUser(1);
-console.log(user);
+getUser(1, (userObj) => {
+  console.log("User Object", userObj);
+  getRepositories(userObj.gitHubUsername, (repoList) => {
+    console.log("Repositories", repoList);
+  });
+});
 console.log("After");
-
-function getUser(id) {
+function getUser(id, callback) {
   setTimeout(() => {
     console.log("Reading a user from a database...");
-    return { id: id, giHubUsername: "KC Sheng" };
+    callback({ id: id, gitHubUsername: "KC Sheng" });
+  }, 2000);
+}
+
+function getRepositories(userName, callback) {
+  setTimeout(() => {
+    console.log("Getting user's repositories from database...");
+    callback(["repo1", "repo2", "repo3"]);
   }, 2000);
 }
