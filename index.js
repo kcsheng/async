a63353e47d4aa601796fe8a316d8d9ab6c801e3b
kcsheng <kacysheng@mail.com>
2021-06-23 15:13:14 +1000

    Finalise promise consumer code

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-console.log("Before");
-getUser(1, (user) => {
-  getRepositories(user.gitHubUsername, (repos) => {
-    getCommits(repos[0], (commits) => {
-      console.log(commits);
-    });
-  });
-});
-console.log("After");
+// getUser(1, (user) => {
+//   getRepositories(user.gitHubUsername, (repos) => {
+//     getCommits(repos[0], (commits) => {
+//       console.log(commits);
+//     });
+//   });
+// });
+
+// Code that consume promise, which replaces callback hell as above.
+// Each .then will return a promise resove/reject for consumption of next .then
+// The way how a promise pattern work is that it separates the producer (resolve, reject) code and consumer code (.then and .catch), which would previosuly be callback.
+getUser(1)
+  .then((user) => getRepositories(user.gitHubUsername))
+  .then((reposs) => getCommits(repos[0]))
+  .then((commits) => console.log(commits))
+  .catch((err) => console.log(err.message));
 
 function getUser(id) {
   return new Promise((resolve, reject) => {
