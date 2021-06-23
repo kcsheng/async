28da738846a714523b3040a344cde75e3dd89e40
kcsheng <kacysheng@mail.com>
2021-06-23 16:01:11 +1000

    Consume multiple promises at once

--- promise-api.js
+++ promise-api.js
@@ -1,3 +1,18 @@
-// Return any promise that is already rejected with errors
-const p = Promise.reject(new Error("Something went wrong"));
-p.catch((err) => console.log(err));
+// Running multiple promises in parallel, the result is demonstrated with an array
+
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log("Asyn Task 1");
+    reject(new Error("something went wrong"));
+  }, 2000);
+});
+const p2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log("Asyn Task 2");
+    resolve("second value");
+  }, 2000);
+});
+
+Promise.all([p1, p2])
+  .then((result) => console.log(result))
+  .catch((err) => console.log(err));
